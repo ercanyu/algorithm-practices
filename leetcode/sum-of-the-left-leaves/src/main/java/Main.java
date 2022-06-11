8897ada7d3fbed079c931f6d023e4193356eddfb
ercaany <ercaany@gmail.com>
2022-06-11 14:22:03 +0300

    LeetCode -> 404. Sum of Left Leaves

---
+++ leetcode/sum-of-the-left-leaves/src/main/java/Main.java
@@ -0,0 +1,40 @@
+package main.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        System.out.println(sumOfLeftLeaves(node));
+    }
+
+    public static int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
